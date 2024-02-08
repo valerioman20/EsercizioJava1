c95ca0f2d989e79fa8af28ad32a7ab1b708aaef4
valerioman20 <valerioman32@gmail.com>
2024-02-09 00:42:09 +0100

    Esercizio completo

---
+++ script.js
@@ -0,0 +1,16 @@
+/* let somma;
+let x = 12;
+let y = 20;
+somma = (x+y)
+console.log(somma); */
+
+/*let sottrazione;
+let x = "12";
+let y = 4;
+sottrazione = (x-y);
+console.log (sottrazione);*/
+
+
+let name1 = "john";
+let name2 = "John";
+console.log (name1.toLowerCase === name2.toLowerCase );
